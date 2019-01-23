4bbd9266bd73a5c9c4c0688157361c501b3ecb58
huiyu.chen <huiyu.chen@gareatech.com>
2019-01-23 16:16:43 +0800

    首次提交代码；
    1.parcel@1.7.0 + vue@2.5.22 + vue-router@3.0.2等基础配置；
    2.在最新版本中对vue的语法和es2015语法都有兼容，不需要重新配置.babelrc;
    3.对于这本次提交只是一个单一的一个demo (仅供参考学习parcel+vue)；

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld />
+     -->
+     <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+      /** 组件注册问题 */
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/router.js'
+new Vue({
+    router,
+  render: h => h(App)
+}).$mount('#app')
